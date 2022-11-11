c1b55831d3d9afdb5b0dd1542ce42c7e8621dcb2
9JaHyun <jahyunnine@gmail.com>
2022-11-11 17:48:31 +0900

    feat: 음식 도메인 생성     DO     - 모두 가변 가능성이 있어 var로 표기     - DB 관점에서 PK만 같으면 같은 객체라 볼 수 있기때문에 equals, hashCode를 PK(id)에만 초점을 맞추도록 함.     - 코틀린에서 `@Index` 사용이 쉽지 않아 일단 DDL SQL 상에서 인덱스를 생성하도록 함.
        TODO
        - 애플리케이션 계층을 최대한 외부 라이브러리가 없게끔 하려고 하는데 Food와 FoodJPAEntity를 분리할지 고민해봐야 할 것 같음.
    
    ISSUE #1

---
+++ src/main/kotlin/com/mealfitkotiln/food/domain/Food.kt
@@ -0,0 +1,50 @@
+package com.mealfitkotiln.food.domain
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.Index
+import javax.persistence.Table
+
+@Table(
+    indexes = { @Index(columnList = "foodName") }
+)
+@Entity
+class Food(
+
+    var foodName: String,       // 음식명
+
+    var oneServing: Double = 0.0,  // 1회제공량
+
+    var kcal: Double = 0.0,     // 칼로리
+
+    var carbs: Double = 0.0,    // 탄수화물
+
+    var protein: Double = 0.0,  // 단백질
+
+    var fat: Double = 0.0,      // 지방
+
+    var madeBy: String,         // 제조사
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Food
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id?.hashCode() ?: 0
+    }
+}
