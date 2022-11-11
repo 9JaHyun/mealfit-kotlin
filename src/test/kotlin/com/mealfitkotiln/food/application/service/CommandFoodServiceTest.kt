e82643bf4f5ae761fb6d35fc3968ddf1c94edd1d
9JaHyun <jahyunnine@gmail.com>
2022-11-11 17:55:33 +0900

    feat: 음식 입력 및 수정과 조회 기능 테스트 코드 작성     DO     - 애플리케이션 영역의 서비스 코드 작성 완료     - Repository의 경우 접근제한자를 internal로 최소화했기 때문에 service 자체에서 검증
    ISSUE #1

---
+++ src/test/kotlin/com/mealfitkotiln/food/application/service/CommandFoodServiceTest.kt
@@ -0,0 +1,45 @@
+package com.mealfitkotiln.food.application.service
+
+import com.mealfitkotiln.food.application.port.`in`.FoodSaveRequestDto
+import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommandFoodServiceTest @Autowired constructor(
+    private val commandFoodService: CommandFoodService,
+    private val queryFoodService: QueryFoodService
+) {
+
+    @Nested
+    @DisplayName("saveFood() 메서드는")
+    inner class Testing_saveFood {
+
+        @DisplayName("FoodSaveRequestDto가 완전히 입력되면 성공")
+        @Test
+        fun saveFood_success() {
+            // given
+            val requestDto = FoodSaveRequestDto("사과", 100.0,
+                80.0, 15.0, 1.0, 1.0, "전국")
+
+            // when
+            commandFoodService.saveFood(requestDto)
+
+            // then
+            val findResult = queryFoodService.getFoodById(1L)
+            assertThat(findResult.foodId).isEqualTo(1)
+            assertThat(findResult.foodName).isEqualTo("사과")
+            assertThat(findResult.oneServing).isEqualTo(100.0)
+            assertThat(findResult.kcal).isEqualTo(80.0)
+            assertThat(findResult.carbs).isEqualTo(15.0)
+            assertThat(findResult.protein).isEqualTo(1.0)
+            assertThat(findResult.fat).isEqualTo(1.0)
+            assertThat(findResult.madeBy).isEqualTo("전국")
+        }
+    }
+
+
+}
---
+++ src/test/kotlin/com/mealfitkotiln/food/application/service/QueryFoodServiceTest.kt
@@ -0,0 +1,70 @@
+성package com.mealfitkotiln.food.application.service
+
+import com.mealfitkotiln.food.application.port.`in`.FoodInfoRequestDto
+import com.mealfitkotiln.food.application.port.`in`.FoodSaveRequestDto
+import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QueryFoodServiceTest @Autowired constructor(
+    private val commandFoodService: CommandFoodService,
+    private val queryFoodService: QueryFoodService
+) {
+    @Nested
+    @DisplayName("findFoods() 메서드는")
+    inner class Testing_findFoods {
+
+        @DisplayName("FoodSaveRequestDto가 완전히 입력되면 성공")
+        @Test
+        fun findFoods_success() {
+            // given
+            commandFoodService.saveFood(FoodSaveRequestDto("음식1", 100.0, 150.0, 30.0, 10.0, 11.0, "제조사1"))
+            commandFoodService.saveFood(FoodSaveRequestDto("음식2", 110.0, 200.0, 35.0, 15.0, 16.0, "제조사2"))
+            commandFoodService.saveFood(FoodSaveRequestDto("음식3", 120.0, 300.0, 40.0, 20.0, 21.0, "제조사3"))
+
+            // when
+            val findResult =
+                queryFoodService.getFoodsByName(FoodInfoRequestDto("음식", 10, "id", false, 10))
+
+            // then
+            assertThat(findResult).hasSize(3)
+            assertThat(findResult).extracting("foodName")
+                .containsExactlyInAnyOrder("음식1", "음식2", "음식3")
+            assertThat(findResult).extracting("oneServing")
+                .containsExactlyInAnyOrder(100.0, 110.0, 120.0)
+            assertThat(findResult).extracting("kcal").containsExactlyInAnyOrder(150.0, 200.0, 300.0)
+            assertThat(findResult).extracting("carbs").containsExactlyInAnyOrder(30.0, 35.0, 40.0)
+            assertThat(findResult).extracting("protein").containsExactlyInAnyOrder(10.0, 15.0, 20.0)
+            assertThat(findResult).extracting("fat").containsExactlyInAnyOrder(11.0, 16.0, 21.0)
+            assertThat(findResult).extracting("madeBy")
+                .containsExactlyInAnyOrder("제조사1", "제조사2", "제조사3")
+        }
+
+        @DisplayName("page size 따라 개수가 달라진다.")
+        @Test
+        fun findFoods_lastId() {
+            // given
+            commandFoodService.saveFood(FoodSaveRequestDto("음식1", 100.0, 150.0, 30.0, 10.0, 11.0, "제조사1"))
+            commandFoodService.saveFood(FoodSaveRequestDto("음식2", 110.0, 200.0, 35.0, 15.0, 16.0, "제조사2"))
+            commandFoodService.saveFood(FoodSaveRequestDto("음식3", 120.0, 300.0, 40.0, 20.0, 21.0, "제조사3"))
+
+            // when
+            val findResult =
+                queryFoodService.getFoodsByName(FoodInfoRequestDto("음식", 2, "id", false, 10))
+
+            // then
+            assertThat(findResult).hasSize(2)
+            assertThat(findResult).extracting("foodName").containsExactlyInAnyOrder("음식3", "음식2")
+            assertThat(findResult).extracting("oneServing").containsExactlyInAnyOrder(120.0, 110.0)
+            assertThat(findResult).extracting("kcal").containsExactlyInAnyOrder(300.0, 200.0)
+            assertThat(findResult).extracting("carbs").containsExactlyInAnyOrder(40.0, 35.0)
+            assertThat(findResult).extracting("protein").containsExactlyInAnyOrder(20.0, 15.0)
+            assertThat(findResult).extracting("fat").containsExactlyInAnyOrder(21.0, 16.0)
+            assertThat(findResult).extracting("madeBy").containsExactlyInAnyOrder("제조사3", "제조사2")
+        }
+    }
+}
