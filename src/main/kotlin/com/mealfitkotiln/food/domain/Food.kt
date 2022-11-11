4ecbc19b0fcb441871ca1676bdf930d5b8154eee
9JaHyun <jahyunnine@gmail.com>
2022-11-11 22:46:36 +0900

    feat: 음식 도메인 인덱스     - 이전에 언급한 음식 도메인의 인덱스가 삭제가 되지 않은 것을 발견했습니다.     - 이를 삭제조치했습니다. ISSUE #1

--- src/main/kotlin/com/mealfitkotiln/food/domain/Food.kt
+++ src/main/kotlin/com/mealfitkotiln/food/domain/Food.kt
@@ -4,12 +4,7 @@ import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
-import javax.persistence.Index
-import javax.persistence.Table
 
-@Table(
-    indexes = { @Index(columnList = "foodName") }
-)
 @Entity
 class Food(
 
