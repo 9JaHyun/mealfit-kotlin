08b1ee4ec345d7881c4b6ff81eceb155c38b3647
9JaHyun <jahyunnine@gmail.com>
2022-11-11 14:49:11 +0900

    feat: project 생성 및 의존성 설 - 프로젝트 생성 - 의존성 설정   - spring 관련 의존성 플러그인 설정   - JPA 관련 의존성 및 플러그인 설정   - Jackson Mapping 문제로 jackson-module-kotlin 의존성 추가

---
+++ src/main/kotlin/com/mealfitkotiln/MealfitKotilnApplication.kt
@@ -0,0 +1,11 @@
+package com.mealfitkotiln
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MealfitKotilnApplication
+
+fun main(args: Array<String>) {
+    runApplication<MealfitKotilnApplication>(*args)
+}
