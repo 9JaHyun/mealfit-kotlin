060a38831d914086d43a20ab203147976a2114f9
9JaHyun <jahyunnine@gmail.com>
2022-11-12 23:24:38 +0900

    docs: CommandFoodPort.deleteAll() 문서화 - 클라이언트에게 제공하는 기능이 아닌만큼 API Docs를 추가했습니다.

--- src/main/kotlin/com/mealfitkotiln/food/application/port/out/CommandFoodPort.kt
+++ src/main/kotlin/com/mealfitkotiln/food/application/port/out/CommandFoodPort.kt
@@ -8,5 +8,12 @@ interface CommandFoodPort {
 
     fun updateFood(food: Food)
 
+    /**
+     * @author 9JaHyun
+     * @desc 테스트 도중 원활한 DB 초기화를 위해 deleteAll() 메서드를 추가했습니다.
+     *       클라이언트들에게 제공하는 메서드가 아니기 때문에 주의를 해서 사용해야 합니다.
+     *       현재 임시방편으로 만들어 놓았기 때문에 추후 deprecated 될 수 있습니다.
+     * @since 1.0
+     */
     fun deleteAll()
 }
