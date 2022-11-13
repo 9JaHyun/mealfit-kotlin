c42e11e5478a7304ce41595d5e925f6ca6590a81
9JaHyun <jahyunnine@gmail.com>
2022-11-13 22:35:19 +0900

    feat: Spring RestDocs 추가 - Spring RestDocs 기능을 추가했습니다.     - 기존 mockito의 `any()`가 nullable해 사용에 어려움이 발생해 mockito-kotlin 의존성 추가     - `build/generated-snippets`에 ascidocs 스니펫 생성되도록 스크립트 설정 - 간단하게 FoodControllerTest.kt 를 테스트로 진행해보았습니다. ISSUE #7

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.7.4"
     id("io.spring.dependency-management") version "1.0.14.RELEASE"
+    id("org.asciidoctor.convert") version "1.5.9.2"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
     kotlin("plugin.jpa") version "1.6.21"
@@ -31,6 +32,15 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     // kotlin의 경우 jackson이 제대로 읽지 못해서 의존성 추가 필요
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
+
+    // https://mvnrepository.com/artifact/javax.validation/validation-api
+    implementation("javax.validation:validation-api:2.0.1.Final")
+
+    // REST DOC
+    testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+    asciidoctor("org.springframework.restdocs:spring-restdocs-asciidoctor")
+
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.h2database:h2")
@@ -47,4 +57,37 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+var snippetsDir by extra { file("build/generated-snippets") }
+
+tasks.test {
+    outputs.dir(snippetsDir)
+}
+
+tasks.asciidoctor {
+    inputs.dir(snippetsDir)
+
+    dependsOn(tasks.test) // 변경
+
+    doFirst {
+        delete {
+            file("src/main/resources/static/docs")
+        }
+    }
+}
+
+tasks.register("copyHTML", Copy::class) { // 3
+    dependsOn(tasks.asciidoctor)
+    from(file("build/asciidoc/html5"))
+    into(file("src/main/resources/static/docs"))
+}
+
+tasks.build { // 4
+    dependsOn(tasks.getByName("copyHTML"))
+}
+
+tasks.bootJar { // 5
+    dependsOn(tasks.asciidoctor)
+    dependsOn(tasks.getByName("copyHTML"))
 }
